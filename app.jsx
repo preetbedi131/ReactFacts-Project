616259f276724494698150bfe34c5704acd9cc15
techgirl19 <pb362004@gmail.com>
2025-07-22 17:09:09 +0530

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar"
+import Main from "./components/main"
+export default function App(){
+return(
+    <>
+            <Navbar />
+            <Main />
+        </>
+
+    )
+
+}
---
+++ components/main.jsx
@@ -0,0 +1,23 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="lists">
+                <li>Was created at Facebook during a hackathon by Jordan Walke.</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>It uses a Virtual DOM to update the UI efficiently.</li>
+                <li>The spinning atom logo reflects React’s atomic, component-based design.</li>
+
+
+
+
+
+
+
+
+
+            </ul>
+        </main>
+    )
+}
